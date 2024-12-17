106542c74bd33fbbc52c91f89a4d0d2cd7260845
angelogentileiii <angelo.john.gentile@gmail.com>
2024-12-17 14:06:33 -0500

    added priority queue solution to problem 2182

--- Medium-Problems/2182-Construct-String-Repeat-Limit/construct-string-repeat-limit.py
+++ Medium-Problems/2182-Construct-String-Repeat-Limit/construct-string-repeat-limit.py
@@ -91,6 +91,84 @@ def repeatLimitedString(s: str, repeatLimit: int) -> str:
     return "".join(result)
 
 
+# ---------------------------------------------------------------------------------------------------------------------------
+
+# THIS SOLUTION USES A PRIORITY QUEUE AND MAP TO TRACK THE CHARACTERS AND ADD INTO THE RESULT STRING
+
+from heapq import heapify, heappop, heappush
+from collections import Counter
+
+
+def repeatLimitedStringHeap(s: str, repeatLimit: int) -> str:
+    # Create a counter map to track each character and it's appearances within the string
+    freq = Counter(s)
+    print(f"FREQ: {freq}")
+
+    # Create a priority queue (Minimum Heap)
+    # --> Lexicographical order means the smallest should be at the top of the heap
+    # --> ord returns the unicode point of each character so ord('a') - ord(char) is 0 - unicode point (ex: z would equal -25 --> Top of MinHeap)
+    pq = [(ord("a") - ord(char), count) for char, count in freq.items()]
+    heapify(pq)
+    print(
+        f"PQ: {pq} --> WITH CHAR: {[(chr(ord('a') - priority), count) for priority, count in pq]}"
+    )
+
+    # Empty result to store the characters for our completed string
+    result: list[str] = []
+
+    # Loop exists as long as there are elements within the priority queue
+    while pq:
+        # Pop the lexicographically largest element (smallest unicode) from the heap with it's count integer
+        char, count = heappop(pq)
+        print(f"CHAR AND COUNT: {(chr(ord('a') - char), count)}")
+
+        # Find the maximum amount that character can be repeated in the result string --> Limited by frequency or by repeatLimit
+        repeat = min(repeatLimit, count)
+        print(f"Max Repeat: {repeat}")
+
+        # Append that character as many times as the calculated maximum
+        result.append(chr(ord("a") - char) * repeat)
+        print(f"RESULT: {result}")
+
+        # Reduce the count of that character by the amount that has been inserted into the result string
+        count -= repeat
+        print(f"UPDATE COUNT: {count}")
+
+        # If there are still more of the same character available and the queue is not empty
+        if count > 0 and pq:
+            # We move to the next character by popping from the priority queue (the next lexicographically largest character and count)
+            next_char, next_count = heappop(pq)
+            print(f"NEXT CHAR AND COUNT: {(chr(ord('a') - next_char), next_count)}")
+
+            # We append one of that character to the string
+            result.append(chr(ord("a") - next_char))
+
+            # If there are more of that character available, we push it into the heap with a reduced count
+            if next_count > 1:
+                heappush(pq, (next_char, next_count - 1))
+
+            # Then we push our prior character into the heap with a reduced count as well
+            heappush(pq, (char, count))
+
+            # By doing the above, we are able to alternate between the largest lexicographical characters until they are dispersed properly
+            print(
+                f"CURRENT HEAP STATUS: {[(chr(ord('a') - priority), count) for priority, count in pq]}"
+            )
+
+        print(f"CURRENT ITERATION: {result}")
+
+    print(f'ANSWER: {"".join(result)}')
+    print(" ")
+
+    # Join the result array and return the completed string
+    return "".join(result)
+
+
 repeatLimitedString("cczazcc", 3)
 
 repeatLimitedString("aababab", 2)
+
+print("-" * 35)
+
+repeatLimitedStringHeap("cczazcc", 3)
+repeatLimitedStringHeap("aababab", 2)
