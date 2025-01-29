cf527b43b9cbde73d86f39a49c2ccf2cd4f3523f
angelogentileiii <angelo.john.gentile@gmail.com>
2025-01-29 10:30:14 -0500

    added comments / notes to problem #2658

--- Medium-Problems/2658-Max-Fish-In-Grid/max-fish-in-grid.py
+++ Medium-Problems/2658-Max-Fish-In-Grid/max-fish-in-grid.py
@@ -16,6 +16,13 @@
 
 # THOUGHTS
 
+# We create a helper function that when we choose a particular cell, we check all possibly directions for a non-zero value
+#   - This helper function will be called recursively, so that we then check subsequent combinations to find the total from a particular path
+
+# We initialize an array to store whether or not a cell has already been visited --> To avoid overlapping on our path
+
+# With each choice, we need to compare the total fish available with our maximum fish currently found
+#   - By the end of our grid, we will have the maximum-fish by choosing the optimal beginning cell within the grid
 
 # ---------------------------------------------------------------------------------------------------------------------------
 
@@ -24,24 +31,31 @@ def findMaxFish(grid: list[list[int]]) -> int:
     print("Initial Grid:")
     print("\n".join(str(row) for row in grid))
 
+    # Initialize variables for our total rows and total columns
     rows = len(grid)
     cols = len(grid[0])
 
+    # Initialize a grid that tracks whether or not a cell has been visited in our recursive function call --> Utilize booleans but could be integers as well
     visited = [[False] * cols for _ in range(rows)]
 
     print(f"\nInitial Visited Array: ")
     print("\n".join(str(row) for row in visited))
 
+    # Initialize variable that will store our maximum fish accumulated --> Updates will be made at each cell choice that leads to an increased number of fish
     max_fish = 0
 
+    # Iterate through our entire grid (cell by cell)
     for row in range(rows):
         for col in range(cols):
+            # Check if the current cell has fish (val greater than 0) and it has not been visited previously (it is not part of another path already traversed)
             if grid[row][col] > 0 and not visited[row][col]:
-                total_fish = calcFish(grid, visited, row, col)
+                # Call our helper function on each valid starting cell in the grid --> Will not call on water cells or cells already visited as noted
+                curr_total = calcFish(grid, visited, row, col)
 
-                print(f"\nTotal Fish for Choice: -- {total_fish} --")
+                print(f"\nTotal Fish for Choice: -- {curr_total} --")
 
-                max_fish = max(max_fish, total_fish)
+                # Check the maximum fish against the current total from the cell chosen --> Update max_fish accordingly
+                max_fish = max(max_fish, curr_total)
 
     print(f"\nCompleted Visited Array: ")
     print("\n".join(str(row) for row in visited))
@@ -50,31 +64,39 @@ def findMaxFish(grid: list[list[int]]) -> int:
     return max_fish
 
 
+# Helper function for checking path of fish within grid and returning the total fish available to a chosen cell path
 def calcFish(
     grid: list[list[int]], visited: list[list[int]], row: int, col: int
 ) -> int:
+
+    # Conditional statement that rules out all cases where the path would break
     if (
-        row < 0
-        or row >= len(grid)
-        or col < 0
-        or col >= len(grid[0])
-        or grid[row][col] == 0
-        or visited[row][col]
+        row < 0  # We have extended out of bounds (low end)
+        or row >= len(grid)  # We have extended out of bounds (high end)
+        or col < 0  # We have extended out of bounds (low end)
+        or col >= len(grid[0])  # We have extended out of bounds (high end)
+        or grid[row][col] == 0  # There are no fish in the current cell
+        or visited[row][col]  # The cell has already been visited
     ):
         return 0
 
+    # Mark the current cell as visited in our visited graph
     visited[row][col] = True
 
+    # Set the total fish for this path to our beginning cell --> This is what our minimum for this path will be
     total_fish = grid[row][col]
 
     print(f"\nIteration Visited Array: {(row, col)} - Fish: {total_fish}")
     print("\n".join(str(row) for row in visited))
 
+    # Each tuple contains possible directions to check for our path --> Recursive calls of this function
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+    # For each direction, we call this function and add the fish value to the total_fish for this path --> Either 0 because we have hit our conditional or the value of the cell itself
     for dr, dc in directions:
         total_fish += calcFish(grid, visited, row + dr, col + dc)
 
+    # After the loop, the final return value of total_fish will represent the total fish for the path available from the original cell chosen
     return total_fish
 
 
--- Medium-Problems/2658-Max-Fish-In-Grid/max-fish-in-grid.ts
+++ Medium-Problems/2658-Max-Fish-In-Grid/max-fish-in-grid.ts
@@ -16,16 +16,25 @@
 
 // THOUGHTS
 
+// We create a helper function that when we choose a particular cell, we check all possibly directions for a non-zero value
+//  - This helper function will be called recursively, so that we then check subsequent combinations to find the total from a particular path
+
+// We initialize an array to store whether or not a cell has already been visited --> To avoid overlapping on our path
+
+// With each choice, we need to compare the total fish available with our maximum fish currently found
+//  - By the end of our grid, we will have the maximum-fish by choosing the optimal beginning cell within the grid
+
 // ---------------------------------------------------------------------------------------------------------------------------
 
 function findMaxFish(grid: number[][]): number {
     console.log("Initial Grid:");
     grid.forEach((row) => console.log(row));
 
+    // Initialize variables for our total rows and total columns
     const rows = grid.length;
     const cols = grid[0].length;
 
-    // Initialize the visited array
+    // Initialize a grid that tracks whether or not a cell has been visited in our recursive function call --> Utilize booleans but could be integers as well
     const visited: boolean[][] = Array.from({ length: rows }, () =>
         Array(cols).fill(false)
     );
@@ -33,15 +42,20 @@ function findMaxFish(grid: number[][]): number {
     console.log("\nInitial Visited Array:");
     visited.forEach((row) => console.log(row));
 
+    // Initialize variable that will store our maximum fish accumulated --> Updates will be made at each cell choice that leads to an increased number of fish
     let maxFish = 0;
 
+    // Iterate through our entire grid (cell by cell)
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < cols; col++) {
+            // Check if the current cell has fish (val greater than 0) and it has not been visited previously (it is not part of another path already traversed)
             if (grid[row][col] > 0 && !visited[row][col]) {
+                // Call our helper function on each valid starting cell in the grid --> Will not call on water cells or cells already visited as noted
                 const totalFish = calcFish(grid, visited, row, col);
 
                 console.log(`\nTotal Fish for Choice: -- ${totalFish} --`);
 
+                // Check the maximum fish against the current total from the cell chosen --> Update maxFish accordingly
                 maxFish = Math.max(maxFish, totalFish);
             }
         }
@@ -55,25 +69,29 @@ function findMaxFish(grid: number[][]): number {
     return maxFish;
 }
 
+// Helper function for checking path of fish within grid and returning the total fish available to a chosen cell path
 function calcFish(
     grid: number[][],
     visited: boolean[][],
     row: number,
     col: number
 ): number {
+    // Conditional statement that rules out all cases where the path would break
     if (
-        row < 0 ||
-        row >= grid.length ||
-        col < 0 ||
-        col >= grid[0].length ||
-        grid[row][col] === 0 ||
-        visited[row][col]
+        row < 0 || // We have extended out of bounds (low end)
+        row >= grid.length || // We have extended out of bounds (high end)
+        col < 0 || // We have extended out of bounds (low end)
+        col >= grid[0].length || // We have extended out of bounds (high end)
+        grid[row][col] === 0 || // There are no fish in the current cell
+        visited[row][col] // The cell has already been visited
     ) {
         return 0;
     }
 
+    // Mark the current cell as visited in our visited graph
     visited[row][col] = true;
 
+    // Set the total fish for this path to our beginning cell --> This is what our minimum for this path will be
     let totalFish = grid[row][col];
 
     console.log(
@@ -81,6 +99,7 @@ function calcFish(
     );
     visited.forEach((row) => console.log(row));
 
+    // Each tuple contains possible directions to check for our path --> Recursive calls of this function
     const directions = [
         [0, 1], // Right
         [0, -1], // Left
@@ -88,14 +107,15 @@ function calcFish(
         [-1, 0], // Up
     ];
 
+    // For each direction, we call this function and add the fish value to the totalFish for this path --> Either 0 because we have hit our conditional or the value of the cell itself
     for (const [dr, dc] of directions) {
         totalFish += calcFish(grid, visited, row + dr, col + dc);
     }
 
+    // After the loop, the final return value of totalFish will represent the total fish for the path available from the original cell chosen
     return totalFish;
 }
 
-// Test cases
 findMaxFish([
     [0, 2, 1, 0],
     [4, 0, 0, 3],
